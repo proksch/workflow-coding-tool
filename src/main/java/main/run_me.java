f7db24bde3f6c2525ad47ba437c26d17dca254c2
Sebastian Proksch <development@mail.proks.ch>
2019-03-22 19:55:24 +0100

    Intermediate snapshot of main script.

--- src/main/java/main/run_me.java
+++ src/main/java/main/run_me.java
@@ -19,40 +19,75 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.BitSet;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Predicate;
 
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.BaseErrorListener;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Parser;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
-import org.antlr.v4.runtime.atn.ATNConfigSet;
-import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.AbstractFileFilter;
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
 
-import parser.PseudoDotLexer;
+import data.Workflow;
+import parser.MyParser;
 import parser.PseudoDotParser;
 import parser.PseudoDotParser.DigraphContext;
 import parser.PseudoDotParser.EdgeContext;
 import parser.PseudoDotParser.GraphsContext;
 
 public class run_me {
+	private static final String ROOT = "/Users/seb/versioned/documents/bart-paper/resources/survey_responses/";
+
+	private static final Set<String> LABELS = readLabels(ROOT + "labels.txt");
+	private static final Set<String> USED_LABELS = new HashSet<String>();
+
+	private static final Map<String, Workflow> joined = readResults(ROOT + "joined/");
+	// private static final Map<String, Workflow> carmine = readResults(ROOT +
+	// "carmine/");
+	private static final Map<String, Workflow> seb = readResults(ROOT + "seb/");
+
 	public static void main(String[] args) throws IOException {
 
-		List<String> labels = readLabels("data/labels.txt");
-		List<String> usedLabels = new LinkedList<>();
-		PseudoDotParser parser = setupParser("data/a/scenario1.dot");
-		System.out.println("-------------------------");
+		// TODO .. do something with data
+
+		System.out.println("Unused labels:");
+		for (String label : LABELS) {
+			if (!USED_LABELS.contains(label)) {
+				System.out.printf("- %s\n", label);
+			}
+		}
+
+	}
+
+	private static Map<String, Workflow> readResults(String path) {
+		System.out.println("searching for results in: " + path);
+		Map<String, Workflow> res = new HashMap<String, Workflow>();
+		Iterator<File> it = findFiles(path, f -> f.endsWith(".dot"));
+		while (it.hasNext()) {
+			File f = it.next();
+			PseudoDotParser parser = MyParser.parse(f);
+			List<Workflow> ws = parseWorkflows(parser, f.getAbsolutePath());
+		}
+
+		return res;
+	}
+
+	private static List<Workflow> parseWorkflows(PseudoDotParser parser, String path) {
+		List<Workflow> ws = new LinkedList<>();
 
 		GraphsContext graphs = parser.graphs();
 
 		for (DigraphContext d : graphs.digraph()) {
-			System.out.printf("## %s ##\n", d.PART_ID().getText());
+			String id = d.ID().getText();
+			System.out.println("ID: " + id);
+			Workflow w = new Workflow(id);
 
 			for (EdgeContext e : d.edge()) {
 				List<TerminalNode> curLabels = e.LABEL();
@@ -67,11 +102,11 @@ public class run_me {
 					for (TerminalNode l : curLabels) {
 						String label = l.getText();
 						label = label.substring(1, label.length() - 1);
-						usedLabels.add(label);
-
-						if (!labels.contains(label)) {
-							System.err.println("Unkown label: " + label);
+						if (!LABELS.contains(label)) {
+							throw new RuntimeException(
+									"Unkown label: " + label + " (" + path + ", " + l.getSourceInterval() + ")");
 						}
+						USED_LABELS.add(label);
 
 						if (last != null) {
 							System.out.printf("%s -> %s;\n", last, label);
@@ -80,91 +115,46 @@ public class run_me {
 					}
 				}
 			}
+			ws.add(w);
 		}
-		System.out.println("-------------------------");
-		System.out.println("Unused labels:");
-		for (String label : labels) {
-			if (!usedLabels.contains(label)) {
-				System.out.printf("- %s\n", label);
-			}
-		}
-
+		return ws;
 	}
 
-	private static PseudoDotParser setupParser(String path) throws IOException {
-		String content = FileUtils.readFileToString(new File(path), UTF_8);
-
-		MyErrorListener el = new MyErrorListener(path);
-		CharStream stream = new ANTLRInputStream(content + "\n");
-		PseudoDotLexer lexer = new PseudoDotLexer(stream);
-		lexer.removeErrorListeners();
-		lexer.addErrorListener(el);
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		PseudoDotParser parser = new PseudoDotParser(tokens);
-		parser.removeErrorListeners();
-		parser.addErrorListener(el);
-		return parser;
+	private static Iterator<File> findFiles(String path, Predicate<String> predicate) {
+		IOFileFilter fileFilter = new AbstractFileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return predicate.test(file.getAbsolutePath());
+			}
+		};
+		IOFileFilter allDirs = FileFilterUtils.trueFileFilter();
+		Iterator<File> it = FileUtils.iterateFiles(new File(path), fileFilter, allDirs);
+		return it;
 	}
 
-	private static List<String> readLabels(String path) throws IOException {
-		List<String> labels = new LinkedList<>();
-		for (String line : FileUtils.readLines(new File(path), UTF_8)) {
-			if (line.contains("#")) {
-				line = line.substring(0, line.indexOf("#"));
+	private static Set<String> readLabels(String path) {
+		try {
+			Set<String> labels = new LinkedHashSet<>();
+			for (String line : FileUtils.readLines(new File(path), UTF_8)) {
+				if (line.contains("#")) {
+					line = line.substring(0, line.indexOf("#"));
+				}
+				line = line.trim();
+
+				if (line.startsWith("-") || line.startsWith("*")) {
+					line = line.substring(1).trim();
+				}
+
+				if (line.isEmpty()) {
+					continue;
+				}
+
+				labels.add(line);
 			}
-			line = line.trim();
 
-			if (line.startsWith("-") || line.startsWith("*")) {
-				line = line.substring(1).trim();
-			}
-
-			if (line.isEmpty()) {
-				continue;
-			}
-
-			labels.add(line);
-		}
-
-		System.out.println("Valid labels:");
-		for (String label : labels) {
-			System.out.printf("- %s\n", label);
-		}
-
-		return labels;
-	}
-
-	private static class MyErrorListener extends BaseErrorListener {
-		private String pathname;
-
-		public MyErrorListener(String pathname) {
-			this.pathname = pathname;
-		}
-
-		@Override
-		public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
-				String msg, RecognitionException e) {
-			throw new RuntimeException(msg + String.format(" (%s:L%d:%d)", pathname, line, charPositionInLine));
-		}
-
-		@Override
-		public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction,
-				ATNConfigSet configs) {
-			System.err.println("reportContextSensitivity");
-			super.reportContextSensitivity(recognizer, dfa, startIndex, stopIndex, prediction, configs);
-		}
-
-		@Override
-		public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact,
-				BitSet ambigAlts, ATNConfigSet configs) {
-			// System.err.println("reportAmbiguity");
-			super.reportAmbiguity(recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs);
-		}
-
-		@Override
-		public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex,
-				BitSet conflictingAlts, ATNConfigSet configs) {
-			// System.err.println("reportAttemptingFullContext");
-			super.reportAttemptingFullContext(recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs);
+			return labels;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
 }
